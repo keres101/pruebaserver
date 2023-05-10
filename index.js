067624abdc1053d3b358f4a93f4ad02465296572
keres101 <keres.kgb@gmail.com>
2023-05-09 22:29:49 -0500

    morgan

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import logger from 'morgan';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(logger('dev'));
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello World' });
